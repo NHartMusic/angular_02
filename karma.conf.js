e2a7315562b021f8d98aaa69132fce32696ae286
Nick Hart <nhartmusic@gmail.com>
2021-04-07 15:29:49 +0200

    installed bootstrap and wiped everything else

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bitch'),
+      dir: require('path').join(__dirname, './coverage/my_app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
